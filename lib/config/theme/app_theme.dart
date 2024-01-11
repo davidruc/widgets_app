92019315bfc6d285353cc79fca4bba7f980d1920
davidruc <davidarueda18@gmail.com>
2024-01-11 11:48:15 -0500

    feat: :lipstick: modify the styles of the appbar

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+//! Por ahora lo vamos a crear constante pero después vamos a permitir que estos colores se vayan editando en tiempo de ejecución del programa
+const colorList = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.deepPurple,
+  Colors.purple,
+  Colors.pink,
+  Colors.orange
+];
+
+
+class AppTheme {
+
+  AppTheme({
+    this.selectedColor = 0
+  }): assert( selectedColor >= 0, 'Selected Color must be greater than 0' ),
+      assert( selectedColor < colorList.length, 'Selected Color must be less or equal than ${colorList.length - 1}' ); 
+  
+  final int selectedColor;
+
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[ selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: false
+    ),
+  );
+
+
+
+
+}
