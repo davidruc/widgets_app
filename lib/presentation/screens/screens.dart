bf8f056caab52530015591645ff4afea187d5188
davidruc <davidarueda18@gmail.com>
2024-01-12 09:47:27 -0500

    feat: :seedling: creation of new files

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,6 +1,11 @@
-//! Cuando una aplicación es muy robusta es recomendable tener un archivo que se encarga de exportar toooodos los screens.
 
+//! Cuando una aplicación es muy robusta es recomendable tener un archivo que se encarga de exportar toooodos los screens.
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
-
+export 'package:widgets_app/presentation/screens/inifinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controlls/ui_controlls_screen.dart';
---
+++ lib/presentation/screens/ui_controlls/ui_controlls_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UiControllsScreen extends StatelessWidget {
+
+  const UiControllsScreen({super.key});
+  
+  static const name = "uiControlls_screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
